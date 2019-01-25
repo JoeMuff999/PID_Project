f0834ec51a2865468d0786a25dbc4f70230987ff
joemuff999 <joeymuffoletto10@gmail.com>
2019-01-25 07:47:25 -0600

    Quality of Life pt.2

--- Satellite/build/root/PID_Project/Satellite/models/ISS/analytical/satellite_analytic.d
+++
@@ -1,8 +0,0 @@
-build/root/PID_Project/Satellite/models/ISS/analytical/satellite_analytic.o: \
- /root/PID_Project/Satellite/models/ISS/analytical/satellite_analytic.c \
- /root/PID_Project/Satellite/models/ISS/analytical/../headers/satellite_analytic.h \
- /root/PID_Project/Satellite/models/ISS/analytical/../headers/satellite.h
-
-/root/PID_Project/Satellite/models/ISS/analytical/../headers/satellite_analytic.h:
-
-/root/PID_Project/Satellite/models/ISS/analytical/../headers/satellite.h:
